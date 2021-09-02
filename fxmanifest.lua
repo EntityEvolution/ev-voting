e577e42ea4240380ebf1b487d265f4894a2bce4e
MSRP <OMGmsebastian@gmail.com>
2021-09-02 12:55:35 -0400

    feat(ui): added main container + json

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+description 'A simple voting resource created by Entity Evolution'
+
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+    
+}
---
+++ html/js/script.js
@@ -0,0 +1,11 @@
+const doc = document;
+
+this.window.addEventListener('load', e => {
+    window.addEventListener('message', e => {
+        switch (e.data.action) {
+            case 'updateInfo':
+
+            break;
+        }
+    })
+})
